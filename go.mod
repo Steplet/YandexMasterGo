db0e1f957dff482ca4e68b673b73930403ae4679
Артём Стеберг <asteberg@list.ru>
2025-09-27 14:46:56 +0300

    test iter

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Steplet/YandexMasterGo
+
+go 1.24.7
--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://srv.msk01.gigacorp.local/_stats")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+}
