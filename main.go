60583ef63cf2e2e4fd109c711b181cd9bfdea32c
Артём Стеберг <asteberg@list.ru>
2025-09-27 15:07:40 +0300

    test iter print params

--- main.go
+++ main.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func main() {
-	resp, err := http.Get("http://srv.msk01.gigacorp.local/_stats")
-	if err != nil {
-		panic(err)
+	for {
+		resp, err := http.Get("http://srv.msk01.gigacorp.local/_stats")
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		paramsList := strings.Split(string(body), ",")
+		fmt.Println(paramsList)
+
+		time.Sleep(10 * time.Millisecond)
 	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(body))
 }
