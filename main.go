caabc38b93e5b62a5271180b2f895b063843bef5
Артём Стеберг <asteberg@list.ru>
2025-09-27 16:53:39 +0300

    test run

--- main.go
+++ main.go
@@ -38,25 +38,26 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		checkValue(serverParams)
+		fmt.Println(serverParams)
+		//checkValue(serverParams)
 
 	}
 }
 
-func checkValue(serverParams ServerParams) {
-	if serverParams.LoadAverage > 30 {
-		fmt.Printf("Load Average is too high: %d\n", serverParams.LoadAverage)
-	}
-	if (serverParams.RAMExp*100)/serverParams.RAMSize < 80 {
-		fmt.Printf("Memory usage too high: %d\n", (serverParams.RAMExp*100)/serverParams.RAMSize)
-	}
-	if (serverParams.DiskExp*100)/serverParams.DiskSize < 90 {
-		fmt.Printf("Free disk space is too low: %d Mb left\n", (serverParams.DiskExp*100)/serverParams.DiskSize)
-	}
-	if (serverParams.NetExp*100)/serverParams.NetSpeed < 90 {
-		fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", (serverParams.NetExp*100)/serverParams.NetSpeed)
-	}
-}
+//func checkValue(serverParams ServerParams) {
+//	if serverParams.LoadAverage > 30 {
+//		fmt.Printf("Load Average is too high: %d\n", serverParams.LoadAverage)
+//	}
+//	if (serverParams.RAMExp*100)/serverParams.RAMSize < 80 {
+//		fmt.Printf("Memory usage too high: %d\n", (serverParams.RAMExp*100)/serverParams.RAMSize)
+//	}
+//	if (serverParams.DiskExp*100)/serverParams.DiskSize < 90 {
+//		fmt.Printf("Free disk space is too low: %d Mb left\n", (serverParams.DiskExp*100)/serverParams.DiskSize)
+//	}
+//	if (serverParams.NetExp*100)/serverParams.NetSpeed < 90 {
+//		fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", (serverParams.NetExp*100)/serverParams.NetSpeed)
+//	}
+//}
 
 func convSliceToParams(stringParams string) (ServerParams, error) {
 	listParams := strings.Split(stringParams, ",")
